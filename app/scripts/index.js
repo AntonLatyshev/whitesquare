c9395f5c1d052eceec08fc0b2579169531055243
Anton Latyshev <gigibuffon2112@gmail.com>
2017-05-29 09:29:06 +0300

    add new project whitesquare

---
+++ app/scripts/index.js
@@ -0,0 +1,26 @@
+import { markupMenu } from './markup-menu';
+markupMenu(window.document);
+
+/*
+  remove when start project*****************************************************
+*/
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'MARKUP-FRAMEWORK'
+    };
+  }
+
+  render() {
+    return (
+      <div className="lead__title">{this.state.name}</div>
+    );
+  }
+}
+ReactDOM.render(<App/>, document.querySelector('.lead__title'));
+/*
+********************************************************************************
+*/
---
+++ gulp/tasks/default.js
@@ -0,0 +1,23 @@
+import gulp from 'gulp';
+import runSequence from 'run-sequence';
+
+gulp.task('default', () => {
+  runSequence(
+    [
+      'png-sprite',
+      'svg-sprite',
+      'fonts',
+      'markup',
+      'php-files',
+      'markup-menu',
+      'scss',
+      'scripts:compile',
+      'images',
+      'components',
+      'static',
+      'favicon'
+    ],
+    'livereload',
+    'watch'
+  );
+});
